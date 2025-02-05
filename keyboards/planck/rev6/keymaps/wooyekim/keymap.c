5e4ae54f0b2aa02f610304f0e475b2943cd03845
Wooyeol Kim <wooyekim@gmail.com>
2025-02-06 01:27:44 +0900

    Update Planck keymap
    Use thumb position keys as Shift and Number layer switch

--- keyboards/planck/rev6/keymaps/wooyekim/keymap.c
+++ keyboards/planck/rev6/keymaps/wooyekim/keymap.c
@@ -37,10 +37,9 @@ enum custom_keycodes {
   UDS_TLD,
 };
 
-#define NUM MO(LAYER_NUM)
-#define NAV MO(LAYER_NAV)
 #define SPC_SFT LSFT_T(KC_SPC)
-#define ENT_SFT RSFT_T(KC_ENT)
+#define TAB_NAV LT(LAYER_NAV, KC_TAB)
+#define ENT_NUM LT(LAYER_NUM, KC_ENT)
 #define ESC_CTL LCTL_T(KC_ESC)
 #define COLEMAK DF(LAYER_COLEMAK)
 #define QWERTY  DF(LAYER_QWERTY)
@@ -50,74 +49,74 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty (with P/; swapped)
  * ,-----------------------------------------------------------------------------------.
- * | _/~  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   ;  | Bksp |
+ * | _/~  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   ;  |  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |Ct/Esc|   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   P  |  '   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | [    |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |  ]   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | (    | Tab  | GUI  | Alt  |SpcSft|  Nav |  Num |EntSft|Hangul| Hanja|   \  |  )   |
+ * | (    | Meta | Alt  |   -  |SpcSft|TabNav| Bksp |EntNum|   =  |Hangul| Hanja|  )   |
  * `-----------------------------------------------------------------------------------'
  */
 [LAYER_QWERTY] = LAYOUT_planck_grid(
-    UDS_TLD, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_SCLN, KC_BSPC,
+    UDS_TLD, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_SCLN, KC_BSLS,
     ESC_CTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_P,    KC_QUOT,
     KC_LBRC, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RBRC,
-    KC_LPRN, KC_TAB,  KC_LGUI, KC_LALT, SPC_SFT, NAV,     NUM,     ENT_SFT, SC_HAEN, SC_HANJ, KC_BSLS, KC_RPRN
+    KC_LPRN, KC_LGUI, KC_LALT, KC_MINS, SPC_SFT, TAB_NAV, KC_BSPC, ENT_NUM, KC_EQL,  SC_HAEN, SC_HANJ, KC_RPRN
 ),
 
 /* Tarmak-DH 4
  * ,-----------------------------------------------------------------------------------.
- * | _/~  |   Q  |   W  |   F  |   P  |   B  |   J  |   U  |   I  |   Y  |   ;  | Bksp |
+ * | _/~  |   Q  |   W  |   F  |   P  |   B  |   J  |   U  |   I  |   Y  |   ;  |  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |Ct/Esc|   A  |   R  |   S  |   T  |   G  |   M  |   N  |   E  |   L  |   O  |  '   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |  [   |   Z  |   X  |   C  |   D  |   V  |   K  |   H  |   ,  |   .  |   /  |  ]   |
+ * | [    |   Z  |   X  |   C  |   D  |   V  |   K  |   H  |   ,  |   .  |   /  |  ]   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | (    | Tab  | GUI  | Alt  |SpcSft|  Nav |  Num |EntSft|Hangul| Hanja|   \  |  )   |
+ * | (    | Meta | Alt  |   -  |SpcSft|TabNav| Bksp |EntNum|   =  |Hangul| Hanja|  )   |
  * `-----------------------------------------------------------------------------------'
  */
 [LAYER_COLEMAK] = LAYOUT_planck_grid(
-    UDS_TLD, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_U,    KC_I,    KC_Y,    KC_SCLN, KC_BSPC,
+    UDS_TLD, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_U,    KC_I,    KC_Y,    KC_SCLN, KC_BSLS,
     ESC_CTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_L,    KC_O,    KC_QUOT,
     KC_LBRC, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RBRC,
-    KC_LPRN, KC_TAB,  KC_LGUI, KC_LALT, SPC_SFT, NAV,     NUM,     ENT_SFT, SC_HAEN, SC_HANJ, KC_BSLS, KC_RPRN
+    KC_LPRN, KC_LGUI, KC_LALT, KC_MINS, SPC_SFT, TAB_NAV, KC_BSPC, ENT_NUM, KC_EQL,  SC_HAEN, SC_HANJ, KC_RPRN
 ),
 
-/* Lower
+/* Number 
  * ,-----------------------------------------------------------------------------------.
- * |      |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
+ * |  `   |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
+ * | Ctrl |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | [    |      |      |      |      |      |      |   -  |   =  |   +  |   /  | ]    |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | (    |      | GUI  | Alt  |SpcSft|Adjust| Hold |EntSft|      |      |   |  | )    |
+ * |      | Meta | Alt  |      | Space|Adjust| Bksp | Hold |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [LAYER_NUM] = LAYOUT_planck_grid(
-    XXXXXXX, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_LCTL, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    XXXXXXX,
-    KC_LBRC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MINS, KC_EQL,  KC_PLUS, KC_SLSH, KC_RBRC,
-    KC_LPRN, XXXXXXX, KC_LGUI, KC_LALT, SPC_SFT, _______, __HLD__, ENT_SFT, XXXXXXX, XXXXXXX, KC_PIPE, KC_RPRN
+    KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, XXXXXXX,
+    KC_LCTL, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, KC_LGUI, KC_LALT, XXXXXXX, KC_SPC,  _______, KC_BSPC, __HLD__, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
-/* Raise
+/* Navigation
  * ,-----------------------------------------------------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * `-----------------------------------------------------------------------------------'
- * |      | GUI  | Alt  | Ctrl | Shift|      | Left | Down |  Up  | Right|      |      |
+ * |      | Meta | Alt  | Ctrl | Shift|      | Left | Down |  Up  | Right|      |      |
  * `-----------------------------------------------------------------------------------'
  * |      |      |      |      |      |      | Home | PgDn | PgUp | End  |      |      |
  * `-----------------------------------------------------------------------------------'
- * |      |      |      |      |      | Hold |Adjust|      |      |      |      |      |
+ * |      |      |      |      |      | Hold |      |Adjust|      | Clmk | Qwty |  OS  |
  * `-----------------------------------------------------------------------------------'
  */
 [LAYER_NAV] = LAYOUT_planck_grid(
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
     XXXXXXX, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, XXXXXXX,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, __HLD__, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, __HLD__, XXXXXXX, _______, XXXXXXX, COLEMAK, QWERTY,  SWAP_OS
 ),
 
 /* Adjust (Lower + Raise)
@@ -125,18 +124,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |      | Reset|Debug | RGB  |RGBMOD| HUE+ | HUE- | SAT+ | SAT- |BRGTH+|BRGTH-|      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |      |      | Clmk | Qty  |  OS  |      |      |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      | Hold | Hold |      |      |      |      |      |
+ * |      |      |      |      |      | Hold |      | Hold |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [LAYER_ADJUST] = LAYOUT_planck_grid(
     XXXXXXX, RESET,   DB_TOGG, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, COLEMAK, QWERTY,  SWAP_OS, XXXXXXX, XXXXXXX, 
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, __HLD__, __HLD__, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, __HLD__, XXXXXXX, __HLD__, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 )
 
 };
