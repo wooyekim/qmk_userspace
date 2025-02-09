e5909a5840af693486a6ddbf26b19adc158cc12a
Wooyeol Kim <wooyekim@gmail.com>
2025-02-10 01:30:02 +0900

    Fix issue in planck
    Use TLD_GRV instead of KC_TILD

--- keyboards/planck/rev6/keymaps/wooyekim/keymap.c
+++ keyboards/planck/rev6/keymaps/wooyekim/keymap.c
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_SCLN, KC_DQUO,
     UDS_CTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_P,    KC_EQL,
     LBR_SFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RBR_SFT,
-    KC_LPRN, KC_TILD, KC_LGUI, KC_LALT, TAB_NAV, SPC_NUM, ENT_SFT, BSP_CTL, SC_HAEN, SC_HANJ, KC_BSLS, KC_RPRN
+    KC_LPRN, TLD_GRV, KC_LGUI, KC_LALT, TAB_NAV, SPC_NUM, ENT_SFT, BSP_CTL, SC_HAEN, SC_HANJ, KC_BSLS, KC_RPRN
 ),
 
 /* Tarmak-DH 4
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_U,    KC_I,    KC_Y,    KC_SCLN, KC_DQUO,
     UDS_CTL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_L,    KC_O,    KC_EQL,
     LBR_SFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, RBR_SFT,
-    KC_LPRN, KC_TILD, KC_LGUI, KC_LALT, TAB_NAV, SPC_NUM, ENT_SFT, BSP_CTL, SC_HAEN, SC_HANJ, KC_BSLS, KC_RPRN
+    KC_LPRN, TLD_GRV, KC_LGUI, KC_LALT, TAB_NAV, SPC_NUM, ENT_SFT, BSP_CTL, SC_HAEN, SC_HANJ, KC_BSLS, KC_RPRN
 ),
 
 /* Number 
